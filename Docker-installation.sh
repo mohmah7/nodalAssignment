f18a1f3bfec2472e399455faabe8e1426b783258
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-23 08:34:25 +0200

    Update Docker-installation.sh

--- Docker-installation.sh
+++ Docker-installation.sh
@@ -16,7 +16,7 @@ sudo apt-get install \
      software-properties-common
      
      
- curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+ sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
  
  sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
